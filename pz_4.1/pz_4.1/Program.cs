544e9672e126870de3cd1803e2633663be754eb1
Sviatoslav Pohrebnyi <svyatoslav021200@gmail.com>
2024-03-19 22:36:31 +0200

    upgrade to subtask_3.1

--- pz_4.1/pz_4.1/Program.cs
+++ pz_4.1/pz_4.1/Program.cs
@@ -5,3 +5,11 @@ int numChar = 0;
 foreach (char caracters in textInput)
     symbols++;
 Console.WriteLine($" Your text have {symbols} characters.");
+Console.Write(" Which symbol do you want to count? - ");
+char textChar = char.Parse(Console.ReadLine().ToLower());
+foreach (char character in textInput)
+{
+    if (character == textChar)
+        numChar++;
+}
+Console.WriteLine($" This symbol is used {numChar} times in the text.");
