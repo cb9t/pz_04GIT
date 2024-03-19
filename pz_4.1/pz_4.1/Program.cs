72b0865b7f779da772dae7d90745d9b15c00f06b
Sviatoslav Pohrebnyi <svyatoslav021200@gmail.com>
2024-03-19 22:30:24 +0200

    replace code. add subtask_02

--- pz_4.1/pz_4.1/Program.cs
+++ pz_4.1/pz_4.1/Program.cs
@@ -1,5 +1,24 @@
-﻿int[] nums = new int[50];
-for (int i = 2, n = 0; i <= 100; i += 2, n++)
-    nums[n] = i;
-foreach (int i in nums)
-    Console.WriteLine($" Element: {i}");
+﻿int[] randomNums = new int[30];
+int pairNums = 0;
+string[] nums;
+
+for (int i = 0; i < randomNums.Length; i++)
+{
+    Random number = new Random();
+    randomNums[i] = number.Next(0, 322);
+    if ((randomNums[i] % 2) == 0 && randomNums[i] != 0)
+    {
+        pairNums++;
+    }
+}
+
+Console.Write($" Number of even numbers: {pairNums}");
+Console.Write("\n Pair numbers:");
+
+foreach (int n in randomNums)
+    if ((n % 2) == 0 && n != 0)
+    {
+        Console.Write($" {n}");
+        pairNums--;
+        Console.Write(pairNums != 0 ? "," : ".");
+    }
